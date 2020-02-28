846e5ffb32688fd25dc349f7a6a708df5c9c87ac
The Code <everett.code93@gmail.com>
2020-02-27 18:09:53 -0600

    updated code/deleted Develop

--- script.js
+++ script.js
@@ -1,63 +1,24 @@
-function myDate() {
-    var d = Date.now()
-    var n = d.toJSON();
-    var day = $("#currentDay");
-    day.text(n);
-    
+$(document).ready(function(){
+  console.log("loaded")
+  //click function for saving text
+  $(".saveBtn").on("click", function(){
+    console.log("clicked")
+    var id = $(this).attr("id");
+    console.log(id)
+    var text = $(`.${id}`).val();
+    console.log(text)
+    localStorage.setItem(id, text);
+  });
+  //fucntion for retrieving text
+  function getText(){
+    $(".saveBtn").each(function(){
+      var id = $(this).attr("id");
+      var text = localStorage.getItem(id);
+      $(`.${id}`).val(text);
+      
 
-};
+    });
+  };
+  getText();
+});
 
-
-
-
-
-
-
-
-
-
-function getSchedule(){
-    
-    
-    // use JSON.parse and localStorage.get to retrive schedule
-    // create schedule if it doesn't exist already
-    // (moment.js) for each item in schedule set the status to present, past, or future depending on time of day
-    return schedule
-  }
-  function saveSchedule(schedule){
-    // localStorage.setItem and JSON.stringify
-  }
-  function renderSchedule(schedule){
-    // get rid of everything
-    $("#main").empty()
-    // for each hour in the schedule, create a div with the bootstrap class 'row'
-  
-  var pm = ['12','1','2','3','4','5','6','7','8','9','10','11']
-  for(var i = 0; i < pm.length; i++);{
-  var hrs = $("<div>");
-  hrs.addClass("row");
-  hrs.attr("row" + pm[i]);
-  hrs.text(pm[i] + "Pm");
-  $(".row").append(hrs);
-  
-  $("<div>").append(".col");
-  $("<div>").append(".col");
-  $("<div>").append(".col");
-  }
-    // append 3 divs with the bootstrap 'col' classes to the above row 
-    // these keep hour, text, and save button
-    // I color the text column depending on status
-    $("#main").append($("<div>your row goes here<button class='save-btn'>save</button></div>"))
-    // set on click handler for each save button
-    $(".save-btn").click(function(event) {
-      // I have a data attribute called hour on each of my buttons
-      // inside the click handler I navigate the tree using .parent() and .children to find my textarea
-      // and I set the hour to the value of the textarea
-      saveSchedule(schedule)
-    })
-  }
-  function main() {
-    let schedule = getSchedule()
-    renderSchedule(schedule)
-  }
-  $(document).ready(main)
